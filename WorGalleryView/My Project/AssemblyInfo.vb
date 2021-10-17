f38acfcdd0523e7ac306dfe1bafaa0304df605cd
Worcome <worcome.studios@gmail.com>
2021-10-17 05:01:11 +0200

    main

---
+++ WorGalleryView/My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Visualizador de imágenes")>
+<Assembly: AssemblyDescription("Visualizador de imágenes")>
+<Assembly: AssemblyCompany("Worcome Studios")>
+<Assembly: AssemblyProduct("GalleryView")>
+<Assembly: AssemblyCopyright("Copyright © 2021 Worcome Studios. Algunos derechos reservados.")>
+<Assembly: AssemblyTrademark("Worcome Studios")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("5630976d-8e91-4711-98c8-17e7da7b6f5f")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+' mediante el carácter '*', como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("16.0.0.0")>
+<Assembly: AssemblyFileVersion("16.0.0.0")>
+<Assembly: NeutralResourcesLanguage("es")>
